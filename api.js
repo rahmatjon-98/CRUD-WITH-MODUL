614fd0f65a3f5042278eefa900fa918666026008
rahmatjon <tom98john@gmail.com>
2025-05-26 15:33:54 +0500

    all functions work

--- api.js
+++ api.js
@@ -1,10 +1,103 @@
-import getData, { api } from "./dom.js";
+import getData, { api, getUser } from "./dom.js";
 
 async function get() {
   try {
     let { data } = await axios.get(api);
     getData(data);
-  } catch (error) {}
+  } catch (error) {
+    console.error(
+      alert("ошибька соединения: проверьте подключения к интернету")
+    );
+  }
+}
+
+async function deleteUser(id) {
+  try {
+    await axios.delete(`${api}/${id}`);
+    get();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addUSer(newUser) {
+  try {
+    await axios.post(api, newUser);
+    get();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function editUSer(updateUser, idx) {
+  try {
+    let { data } = await axios.put(`${api}/${idx}`, updateUser);
+    console.log(data);
+    get();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function changeStatus(updateUser, id) {
+  try {
+    let { data } = await axios.put(`${api}/${id}`, updateUser);
+    get();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function infoUser(id) {
+  try {
+    let { data } = await axios.get(`${api}/${id}`);
+    getUser(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fSearch(value) {
+  try {
+    let { data } = await axios.get(`${api}?name=${value}`);
+    getData(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fSearchStatus(value) {
+  if (value == "all") {
+    get();
+  } else {
+    try {
+      let { data } = await axios.get(`${api}?status=${value == "active"}`);
+      getData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+async function sortUser() {
+  try {
+    let { data } = await axios.get(api);
+    data.sort((a, b) => a.name.localeCompare(b.name));
+    localStorage.setItem("data", JSON.stringify(data));
+    getData(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default get;
+export {
+  deleteUser,
+  addUSer,
+  changeStatus,
+  editUSer,
+  infoUser,
+  fSearch,
+  fSearchStatus,
+  sortUser,
+};
--- dom.js
+++ dom.js
@@ -1,25 +1,179 @@
 let api = "https://6821ee6ab342dce8004c672c.mockapi.io/api/v1/dataUsers";
-export { api };
+export { api, getUser };
+
+import {
+  addUSer,
+  deleteUser,
+  changeStatus,
+  editUSer,
+  infoUser,
+  fSearch,
+  fSearchStatus,
+  sortUser,
+} from "./api.js";
+
+let body = document.querySelector(".body");
+
 let box = document.querySelector(".box");
+
+let statusSearch = document.querySelector(".statusSearch");
+let inpSearch = document.querySelector(".inpSearch");
+let btnSort = document.querySelector(".btnSort");
+let btnNewUser = document.querySelector(".btnNewUser");
+
+let addDialog = document.querySelector(".addDialog");
+let addForm = document.querySelector(".addForm");
+
+let editDialog = document.querySelector(".editDialog");
+let editForm = document.querySelector(".editForm");
+
+let infoDialog = document.querySelector(".infoDialog");
+let infoImage = document.querySelector(".infoImage");
+let infoName = document.querySelector(".infoName");
+let infoStatus = document.querySelector(".infoStatus");
+let infoAge = document.querySelector(".infoAge");
+let infoId = document.querySelector(".infoId");
+
+let btnaddClose = document.querySelector(".btnaddClose");
+let btneditClose = document.querySelector(".btneditClose");
+let btninfoClose = document.querySelector(".btninfoClose");
+
+let idx = null;
+
+inpSearch.oninput = () => {
+  fSearch(inpSearch.value);
+};
+
+statusSearch.onchange = () => {
+  fSearchStatus(statusSearch.value);
+};
+
+btnSort.onclick = () => {
+  sortUser();
+};
+
+btnaddClose.onclick = () => {
+  addDialog.close();
+};
+btneditClose.onclick = () => {
+  editDialog.close();
+};
+btninfoClose.onclick = () => {
+  infoDialog.close();
+};
+btnNewUser.onclick = () => {
+  addDialog.showModal();
+};
+
+let dark = document.querySelector(".dark");
+let light = document.querySelector(".light");
+
+let darkMode = localStorage.getItem("theme") || "white";
+let fontColor = localStorage.getItem("fontColor") || "black";
+body.style.backgroundColor = darkMode;
+body.style.color = fontColor;
+
+dark.onclick = () => {
+  localStorage.setItem("theme", "black");
+  let darkMode = localStorage.getItem("theme");
+  body.style.backgroundColor = darkMode;
+
+  localStorage.setItem("fontColor", "white");
+  let fontColor = localStorage.getItem("fontColor");
+  body.style.color = fontColor;
+};
+
+light.onclick = () => {
+  localStorage.setItem("theme", "white");
+  let darkMode = localStorage.getItem("theme");
+  body.style.backgroundColor = darkMode;
+
+  localStorage.setItem("fontColor", "black");
+  let fontColor = localStorage.getItem("fontColor");
+  body.style.color = fontColor;
+};
+
+addForm.onsubmit = (event) => {
+  event.preventDefault();
+  let newUser = {
+    image: event.target["addImage"].value,
+    name: event.target["addName"].value,
+    age: event.target["addAge"].value,
+    status: event.target["addStatus"].value == "active",
+    updatedAt: Date.now(),
+  };
+  addUSer(newUser);
+  addDialog.close();
+};
+
+editForm.onsubmit = (event) => {
+  event.preventDefault();
+  let updateUser = {
+    image: event.target["editImage"].value,
+    name: event.target["editName"].value,
+    age: event.target["editAge"].value,
+    status: event.target["editStatus"].value == "active",
+    updatedAt: Date.now(),
+  };
+  editUSer(updateUser, idx);
+  editDialog.close();
+};
 
 export default function getData(data) {
   box.innerHTML = "";
   data.forEach((e) => {
     let div = document.createElement("div");
-    div.classList.add("div")
+    div.classList.add("div");
 
     let h2Name = document.createElement("h2");
-    h2Name.innerHTML = e.name;
+    h2Name.innerHTML = e.name || "none";
 
     let pAge = document.createElement("p");
-    pAge.innerHTML = e.age;
+    pAge.style.padding = " 10px 5px ";
+    pAge.innerHTML = `<b>age:</b> ${e.age}`;
 
-    let pStatus = document.createElement("p");
-    pStatus.innerHTML = e.status ? "active" : "inactive";
+    let pStatus = document.createElement("span");
+    pStatus.classList.add("pStatus");
+    pStatus.style.backgroundColor = e.status ? "green" : "red";
+    pStatus.style.color = "white";
+    pStatus.innerHTML = `<b>status:</b> ${e.status ? "active" : "inactive"}`;
 
     let image = document.createElement("img");
+    image.classList.add("image");
     image.src = e.image;
-    image.style.width = "200px";
+
+    let nameMonth = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    let date = new Date(e.updatedAt);
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    let twoDigits = (n) => n.toString().padStart(2, "0");
+
+    let dateadd = `${twoDigits(day)} ${nameMonth[month]} ${year} ${twoDigits(
+      hours
+    )}:${twoDigits(minutes)}:${twoDigits(seconds)}`;
+
+    let pUpdatedAt = document.createElement("p");
+    pUpdatedAt.innerHTML = `<b>latest changes:</b> ${dateadd}`;
+    pUpdatedAt.style.padding = " 10px 5px ";
 
     let btndele = document.createElement("button");
     btndele.innerHTML = "delete";
@@ -35,11 +189,13 @@ export default function getData(data) {
 
     let check = document.createElement("input");
     check.type = "checkbox";
+    check.classList.add("check");
     check.checked = e.status;
     check.onclick = () => {
       let updateUser = {
         ...e,
         status: !e.status,
+        updatedAt: Date.now(),
       };
       changeStatus(updateUser, e.id);
     };
@@ -51,11 +207,31 @@ export default function getData(data) {
       editForm["editName"].value = e.name;
       editForm["editAge"].value = e.age;
       editForm["editImage"].value = e.image;
+      editForm["editImage"].value = e.image;
       editForm["editStatus"].value = e.status ? "active" : "inactive";
       idx = e.id;
     };
 
-    div.append(image, h2Name, pAge, pStatus, btndele, btnedit, btninfo, check);
+    div.append(
+      image,
+      h2Name,
+      pAge,
+      pStatus,
+      pUpdatedAt,
+      btndele,
+      btnedit,
+      btninfo,
+      check
+    );
     box.append(div);
   });
-}   
+}
+
+function getUser(e) {
+  infoDialog.showModal();
+  infoImage.src = e.image;
+  infoName.innerHTML = `<b>name:</b> ${e.name}`;
+  infoAge.innerHTML = `<b>age:</b> ${e.age}`;
+  infoStatus.innerHTML = `<b>status:</b> ${e.status ? "active" : "inactive"}`;
+  infoId.innerHTML = `<b>id:</b> ${e.id}`;
+}
